b489b2a04a90d51375bb843759a342fa359ab216
Tabea014 <76948160+Tabea014@users.noreply.github.com>
2021-07-08 17:40:35 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import PurpleRose from "./PurpleRose.webp";
+import Dictionary from "./Dictionary";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={PurpleRose} className="PurpleRose img-fluid" alt="PurpleRose" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={PurpleRose} className="PurpleRose img-fluid" alt="PurpleRose" />
+        </header>
+        <main>
+          <Dictionary />
 
-    </div >
+
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Tabea Schuchardt</small>
+        </footer>
+      </div>
+    </div>
   );
 }
 
