65f38143a2872d0ac7a55967ebf319c3a558cc1b
Tabea014 <76948160+Tabea014@users.noreply.github.com>
2021-07-08 16:31:16 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import PurpleRose from "./PurpleRose.webp";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={PurpleRose} className="PurpleRose img-fluid" alt="PurpleRose" />
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
